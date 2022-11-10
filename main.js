85b2485b24cf7ff5c3bd0aa4c9514a54f17a5e8f
luigibustos <louisalphonsobustos@gmail.com>
2022-11-10 12:30:02 -0800

    Main.js File linked to index.html

---
+++ main.js
@@ -0,0 +1,14 @@
+// Game Mech
+//Health = 90 
+// Age = 0
+//Total Point = 0 
+// Hunger 1 - 10
+// Sleepines 1 - 10
+//Border 1 - 10
+// Feed = -1
+// Sleep = - 5
+// Play = - 3
+
+console.log("Sanity Check")
+
+// HTML Elements
