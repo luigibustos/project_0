8a31bdba7fa0b6d96a60c0eb17a110d6946d13cb
luigibustos <louisalphonsobustos@gmail.com>
2022-11-10 12:47:08 -0800

    HTML Elemenst Stored in JS Variables - Tested

--- main.js
+++ main.js
@@ -9,6 +9,26 @@
 // Sleep = - 5
 // Play = - 3
 
-console.log("Sanity Check")
+
 
 // HTML Elements
+
+// Name Buttons
+const nameInput = document.querySelector('#name-input')
+const setName = document.querySelector('#set-name')
+const resetName = document.querySelector('#reset-name')
+
+//Game Buttons
+const feedBtn = document.querySelector('#feed')
+const sleepBtn = document.querySelector('#sleep')
+const playBtn = document.querySelector('#play')
+
+// Game Character Indicators
+const healthStat = document.querySelector('#health-stat')
+const nameStat = document.querySelector('#name-stat')
+const ageStat = document.querySelector('#age-stat')
+const hungerStat = document.querySelector('#hunger-stat')
+const sleepinesStat = document.querySelector('#sleepiness-stat')
+const bordStat = document.querySelector('#bord-stat')
+
+
